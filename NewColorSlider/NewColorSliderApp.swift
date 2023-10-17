7624ca21219ece6b6ec524b6a0271a4fbc57375e
Семен Выдрин <pw5hv7ngcv@privaterelay.appleid.com>
2023-10-17 11:04:24 +0400

    Initial Commit

---
+++ NewColorSlider/NewColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewColorSliderApp.swift
+//  NewColorSlider
+//
+//  Created by Семен Выдрин on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
